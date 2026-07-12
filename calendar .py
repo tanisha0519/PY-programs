30dfcc92aa8e8a0b46cc18fbca470249bc94c7f7
tanisha0519 <tanishas1910@gmail.com>
2026-07-12 11:05:17 +0530

    Add files via upload

---
+++ calendar .py
@@ -0,0 +1,9 @@
+import calendar
+print("===== CALENDAR APPLICATION =====")
+year = int(input("Enter Year (e.g., 2026): "))
+month = int(input("Enter Month (1-12): "))
+if 1 <= month <= 12:
+    print("\nCalendar:\n")
+    print(calendar.month(year, month))
+else:
+    print("Invalid Month! Please enter a month between 1 and 12.")
